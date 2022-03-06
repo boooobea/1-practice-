340ab9787fa39f0daccf297c73df10888ca6cc35
boooobea <childhop90@gmail.com>
2022-03-06 16:48:09 +0900

    ddd

---
+++ src/SamplePackage/SampleGitTest.java
@@ -0,0 +1,10 @@
+package SamplePackage;
+
+public class SampleGitTest {
+	public static void main(String[] args) {
+		
+		System.out.println("github practicing");
+		//git sample test 
+		
+	}//main
+}//end class
