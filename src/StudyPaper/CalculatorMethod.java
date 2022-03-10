e1b7f9cc6f71d3cb7e1b75b7f047a2dd51834a2e
boooobea <childhop90@gmail.com>
2022-03-10 23:16:54 +0900

    Method 기초 이론

---
+++ src/StudyPaper/CalculatorMethod.java
@@ -0,0 +1,25 @@
+package StudyPaper;
+
+public class CalculatorMethod {
+//Method = >리턴타입 메서드이름(){}  // void=return값이 없다.
+	
+		void powerOn() {		
+			System.out.println("전원을 켭니다.");
+		} //poweron
+		
+		int plus(int x, int y) {
+			int result = x + y;
+			return result;
+		}//puls
+		
+		double divide(int x, int y) {
+			double result = (double)x /y; 
+			//피연산자 한쪽이 double형변환 되어 남은피연산자를 자동 형변환
+			return result;
+		}//divide
+		
+		void powerOff() {
+			System.out.println("전원을 끕니다.");
+		}
+ 
+}//end class 
---
+++ src/StudyPaper/ComputerMethod.java
@@ -0,0 +1,37 @@
+package StudyPaper;
+
+import java.util.Arrays;
+
+public class ComputerMethod {
+	//Method(매개변수-가변인자) 
+	
+	int sum1(int[] values) {            //매개변수-배열 
+		int sum = 0;
+		
+//		for(int i=0; i<values.length;i++) {
+//			sum+= values[i];
+//		}//for-		
+		for(int num : values) {
+		sum = sum + num;
+		}//enhanced for-인덱싱으로는 적용할 수 없다.
+		
+		return sum; //메소드 실행 종료 - 변수,리터럴,연산식,다른메소드 호출 가능
+					//하나의 값만 가져올 수 있을때 
+	}//sum1
+	
+	
+	int sum2(int ... values) {          //가변인자(==배열) 를 만들때 사용(...)
+		System.out.println("values : " +Arrays.toString(values));
+		
+		int sum = 0;
+		
+		for(int num : values) {
+			sum += num;
+		}//enhanced for-인덱싱으로는 적용할 수 없다.
+//		for(int i=0; i<values.length; i++) {
+//			sum += values[i];
+//		}//for
+		
+		return sum;
+	}//sum2
+}//end class
