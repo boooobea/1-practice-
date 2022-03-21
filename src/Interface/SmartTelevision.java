3096d49961587d1ececf69d01d83f08bfd745eee
boooobea <childhop90@gmail.com>
2022-03-21 22:46:37 +0900

    implements RemoteControl, Sreachable

---
+++ src/Interface/SmartTelevision.java
@@ -0,0 +1,39 @@
+package Interface;
+
+
+public class SmartTelevision 
+	implements RemoteControl, Sreachable{
+
+	
+	private int volume;
+	
+	
+	@Override
+	public void turnOn() {
+		System.out.println(" TV를 켭니다.");
+	}//turnOn
+	
+	public void turnOff() {
+		System.out.println(" TV를 끕니다.");
+	}//turuOff 
+	
+	public void setVolume(int volume) {
+		//인터페이스에 선언된 static final 상수의 활용
+		if(volume > RemoteControl.MAX_VOULUE) {
+			this.volume = RemoteControl.MAX_VOULUE;
+		} else if(volume < RemoteControl.MIN_VOULUE) {
+			this.volume = RemoteControl.MIN_VOULUE;
+		} else {
+			this.volume = volume;
+		}//if-else
+		System.out.println("현재 TV 볼륨:" + volume);
+	}//setVolume
+	
+	//다른 인터페이스인, Sreachable에 선언된 추상메소의 재정의 
+	@Override
+	public void sreach(String url) {
+		System.out.println(url + "을 검색합니다.");
+	}//sreach
+	
+	
+}//end class
