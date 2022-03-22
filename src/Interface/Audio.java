51633a35f608ae25164f18ec8a38370b0d668f3b
boooobea <childhop90@gmail.com>
2022-03-22 16:26:04 +0900

    implements RemoteControl

--- src/Interface/Audio.java
+++ src/Interface/Audio.java
@@ -32,6 +32,26 @@ public class Audio implements RemoteControl {
 			this.volume = volume;
 		}//if-else
 		System.out.println("현재 Audio 볼륨:" + volume);
+	}
+
+	
+	//3. default method
+	//override할때는 default 키워드를 삭제한다. 
+	@Override
+	public void setMute(boolean mute) {
+		System.out.println("Audio::setMute() invoked.");
 	}//setVolume
+	
+	
+	//static method override
+	/*The method changeBattery() of type Audio must override or implement a supertype method
+	 * 인터페이스를 구현하여도 모든 정적멤버는 소속이 clazz 이기때문에(공통) 오버라이드 할 수 없다. 
+	 */
+//	@Override
+//	static void changeBattery(){
+//		System.out.println("Audio::changeBattery() invoked.");
+//	}//changeBattery
+	
+	
 	
 }//end class
