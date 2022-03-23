0bf8cc13ada76a4e2ec065b4ebdab578c876d9fe
boooobea <childhop90@gmail.com>
2022-03-23 22:44:49 +0900

    Money

---
+++ src/exception/backing/Money.java
@@ -0,0 +1,17 @@
+package exception.backing;
+
+import lombok.Getter;
+
+public class Money {
+		
+	@Getter int amount; //양 
+	
+	
+	public Money(int amount) {
+		this.amount = amount;
+	}//constructor
+	
+	
+	
+	
+}//end class
