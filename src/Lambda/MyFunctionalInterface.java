5bbe06b78a83a10d59d4099ab8896f12bfe28f5e
boooobea <childhop90@gmail.com>
2022-03-25 23:35:25 +0900

    FunctionalInterface(함수적 인터페이스)

---
+++ src/Lambda/MyFunctionalInterface.java
@@ -0,0 +1,16 @@
+package Lambda;
+
+
+//Functional Interface(함수적 인터페이스) 
+
+@FunctionalInterface 
+public interface MyFunctionalInterface {
+
+//	public abstract void method();  		//1.case : 매개변수x, 리턴타입x
+//	public abstract void method2(int age); 	//2.case : 매개변수o, 리턴타입x
+//	public abstract int method3();			//3.case : 매개변수x, 리턴타입o
+	public abstract int method4(int age);	//4.case : 매개변수o, 리턴타입o
+	
+	
+}//interface
+
