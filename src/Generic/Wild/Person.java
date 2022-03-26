824c5fefcaec06b97c755619e33ca8078aab0bd6
boooobea <childhop90@gmail.com>
2022-03-26 23:56:34 +0900

    Person <?>

---
+++ src/Generic/Wild/Person.java
@@ -0,0 +1,17 @@
+package Generic.Wild;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+@Setter
+@Getter
+@AllArgsConstructor
+public class Person {
+
+	private String name;
+	
+	
+}//end class
