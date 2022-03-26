2ad50b07f92ed116578d4fda6bcff3a1159cb37d
boooobea <childhop90@gmail.com>
2022-03-27 00:54:16 +0900

    StorageImple<T> implements Storage<T>

---
+++ src/Generic/Wild/StorageImpl.java
@@ -0,0 +1,26 @@
+package Generic.Wild;
+
+import lombok.ToString;
+
+@ToString
+public class StorageImpl<T> implements Storage<T>{
+	
+	private T[] array;
+
+	
+	public StorageImpl(int capacity) {
+		this.array = (T[])(new Object[capacity]);
+	}//constructor
+
+	
+	@Override
+	public void add(T item, int index) {
+		array[index] = item;
+	}//add
+
+	@Override
+	public T get(int index) {
+		return array[index];
+	}//get
+
+}//end class
