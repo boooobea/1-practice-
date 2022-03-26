2ce47b5f14a86ed4bd25424b3a0749bd7eef5edb
boooobea <childhop90@gmail.com>
2022-03-26 15:24:44 +0900

    Function

---
+++ src/Lambda/API/Studendt.java
@@ -0,0 +1,18 @@
+package Lambda.API;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+
+//Function-1
+public class Studendt {
+
+	private String name;
+	private int englishScore;
+	private int mathScore;
+	
+}//end class
