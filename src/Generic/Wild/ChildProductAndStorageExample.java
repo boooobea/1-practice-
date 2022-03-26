ef842421e7cd22d1f038f71d7d4e76e415adc057
boooobea <childhop90@gmail.com>
2022-03-27 00:54:48 +0900

    제네릭클래스상속 / 제네릭인터페이스구현

---
+++ src/Generic/Wild/ChildProductAndStorageExample.java
@@ -0,0 +1,25 @@
+package Generic.Wild;
+
+
+
+public class ChildProductAndStorageExample {
+
+	public static void main(String[] args) {
+		
+		//제네릭 클래스를 상속받은 클래스 
+		ChildProduct<Tv, String, String> product = new ChildProduct<>();
+		
+		product.setKind(new Tv());
+		product.setModel("smartTV");
+		product.setCompany("samsung");
+		
+//		---
+		//제네릭 인터페이스를 구현한 클래스
+		Storage<Tv> storage = new StorageImpl<>(100);
+		storage.add(new Tv(), 0);
+		Tv tv = storage.get(0);
+		
+		System.out.println(" - storage : " + storage );
+	}//main
+	
+}//end class
