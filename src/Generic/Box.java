52c35d64f82f38639a8f255422c4f33733465f09
boooobea <childhop90@gmail.com>
2022-03-27 00:55:41 +0900

    Generic - Box<T>

--- src/Generic/Box.java
+++ src/Generic/Box.java
@@ -5,17 +5,13 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
-
-//Generic class 
 @ToString
-@Getter
 @Setter
+@Getter
 @NoArgsConstructor
-public class Box<T> {
-
+public class Box<T>{
+	
 	private T t;
 	
-//	public T get() { return t;}			//get
-//	public void set(T t) { this.t = t; }//set
 	
 }//end class
