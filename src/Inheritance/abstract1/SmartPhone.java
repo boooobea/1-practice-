e69c9f8c03b72fd376b72783b42118309763884c
boooobea <childhop90@gmail.com>
2022-03-27 12:34:14 +0900

    extends abstract class

---
+++ src/Inheritance/abstract1/SmartPhone.java
@@ -0,0 +1,16 @@
+package Inheritance.abstract1;
+
+
+
+public class SmartPhone extends Phone {
+
+		public SmartPhone(String owner) {
+			super(owner);
+		}//constructor
+		
+		public void internetSreach() {
+			System.out.println("인터넷 검색을 합니다.");
+		}//internetSreach
+	
+	
+}//end class
