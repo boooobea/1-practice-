edbb535fe7671cb5afc10b23a4f46b6aacb9a14a
boooobea <childhop90@gmail.com>
2022-03-30 19:12:48 +0900

    Comparable 1

---
+++ src/collection/comparable/Fruit.java
@@ -0,0 +1,15 @@
+package collection.comparable;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+
+@ToString
+@AllArgsConstructor
+public class Fruit {			//POJO : Plain Old Java Object
+	
+	public String name;
+	public int price;
+	
+	
+}//end class
