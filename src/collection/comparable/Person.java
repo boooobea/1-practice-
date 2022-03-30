e1af9fb8a30039e92a108f756baf62134cb23bee
boooobea <childhop90@gmail.com>
2022-03-30 19:13:20 +0900

    Comparable 1

---
+++ src/collection/comparable/Person.java
@@ -0,0 +1,21 @@
+package collection.comparable;
+
+import lombok.AllArgsConstructor;
+
+
+
+@AllArgsConstructor
+public class Person implements Comparable<Person>{
+
+	public String name;
+	public int age;
+	
+	
+	@Override
+	public int compareTo(Person o) {
+		if(age < o.age) return -1;
+		else if(age == o.age) return 0;
+		else return 1;
+	}//compareTo
+
+}
