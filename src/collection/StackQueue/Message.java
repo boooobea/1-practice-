aa8f5c1ef29c1a33ace3bfe171cc4412ef282fdd
boooobea <childhop90@gmail.com>
2022-03-30 19:12:05 +0900

    Queue 1

---
+++ src/collection/StackQueue/Message.java
@@ -0,0 +1,13 @@
+package collection.StackQueue;
+
+import lombok.AllArgsConstructor;
+import lombok.ToString;
+
+@AllArgsConstructor
+@ToString
+public class Message {
+
+	public String command;
+	public String to;
+	
+}//end class
