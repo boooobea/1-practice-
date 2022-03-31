7a2c932a2a259f743251cab115037e971f2891f7
boooobea <childhop90@gmail.com>
2022-03-31 19:50:06 +0900

    read()

--- src/IO/ReadExample.java
+++ src/IO/ReadExample.java
@@ -3,8 +3,8 @@ package IO;
 import java.io.FileInputStream;
 import java.io.InputStream;
 
-
-//throw Exception
+ 
+//throw Exception 
 public class ReadExample {
 
 	public static void main(String[] args) throws Exception {
