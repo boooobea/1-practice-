e54955f8f73a4a2a1dc04567cfb9d718ce4d0f64
boooobea <childhop90@gmail.com>
2022-04-06 00:31:03 +0900

    implements Serializable, UID

--- src/IO/Object/ClassC.java
+++ src/IO/Object/ClassC.java
@@ -2,23 +2,21 @@ package IO.Object;
 
 import java.io.Serializable;
 
-import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
 
 @NoArgsConstructor
-public class ClassC implements Serializable{
-	
+public class ClassC implements Serializable {
+
+	//무작위 정수값을 생성하여 넣어준다(compiling)
 	//직렬화할 필요가 있는 클래스를 선언할 때에는, 
-	//자바컴파일러에게 자동추가를 맡기지 않고, 개발자가 직접 필드와 초기값을 지정하여
-	//유연성을 줄 수 있다. 
-	private static final long serialVersionUID = 7777777L;
+	//자바컴파일러에게 자동추가 맡기지 않고 직접 초기화하여 유연성을 이용한다. 
+	// UID가 같으면 필드 내부구성이 바뀌어도 같은 클래스로 판단한다. 
+	private static final long serialVersionUID = 6666L;
 
-	int field1;
+	int field1; 
 	
-	//객체의 직렬화 이후에, 새로운 필드 추가 -> Comfile -> SerialVersionUID 다시 생성 
-	int field2;
-
+	//객체의 직렬화 후 새로운 필드 추가 -> 컴파일 ->UID변경 
+	int field2; 
 	
-	//private static final long serialVersionUID =컴파일러가 무작위정수값을 생성하여 넣어준다.
 }//end class
