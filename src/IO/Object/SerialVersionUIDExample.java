e1fa8a667ade9cfe733f358c62601529b6d6862b
boooobea <childhop90@gmail.com>
2022-04-06 00:31:47 +0900

    객체입출력보조스트림 - SerialVersionUID

--- src/IO/Object/SerialVersionUIDExample.java
+++ src/IO/Object/SerialVersionUIDExample.java
@@ -1,6 +1,5 @@
 package IO.Object;
 
-import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
 
@@ -8,25 +7,23 @@ public class SerialVersionUIDExample {
 
 	public static void main(String[] args) throws Exception {
 		
-		//1.파일에 출력할 출력스트림 생성
+		//1. 파일에 출력할 출력스트림 생성
 		FileOutputStream fos = new FileOutputStream("C:/temp/Object.dat");
 		
-
 		//2. 객체출력보조스트림 생성
 		ObjectOutputStream oos = new ObjectOutputStream(fos);
 		
-		//3. 직렬화할 객체 생성(Serializable) 
+		//3. 직렬화 객체생성(Serializable)
 		ClassC classC = new ClassC();
-		classC.field1 = 1;				//int타입의 인스턴스필드 초기화 
+		classC.field1 = 1;		//int타입의 인스턴스필드 초기화 
 		
 		//4. 객체출력
 		oos.writeObject(classC);
 		
-		
-		//5.자원해제 
+		//5. 자원해제
 		oos.flush();
-		
 		oos.close();
 		fos.close();
+		
 	}//main
 }//end class
