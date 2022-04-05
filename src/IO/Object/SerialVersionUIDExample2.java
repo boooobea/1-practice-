d2f47b94cd98ec89648d48bf86970f65017160cb
boooobea <childhop90@gmail.com>
2022-04-06 00:31:57 +0900

    객체입출력보조스트림-SerialVersionUID

--- src/IO/Object/SerialVersionUIDExample2.java
+++ src/IO/Object/SerialVersionUIDExample2.java
@@ -10,16 +10,20 @@ public class SerialVersionUIDExample2 {
 		
 		//1. 역직렬화할 객체가 저장된 파일에 대한 입력스트림 생성
 		FileInputStream fis = new FileInputStream("C:/temp/Object.dat");
+
 		
-		
-		//2. 입력스트림에 객체 입력 보조스트림 연결
+		//2. 객체입력보조스트림 생성
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		
 		
-		//3. 객체복원(역직렬화 수행) 
+		//3. 객체 복원(역직렬화)
 		ClassC classC = (ClassC) ois.readObject();
 		System.out.println("field1 : " + classC.field1);
 		System.out.println("field2 : " + classC.field2);
 		
+		//4. 자원해제
+		ois.close();
+		fis.close();
+		
 	}//main
-}//end class
+}
