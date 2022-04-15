cab4261ab1c8b8130c5ca22d32b807387f919050
boooobea <childhop90@gmail.com>
2022-04-15 21:57:26 +0900

    CafeOrder ----2

---
+++ src/first/CafeOrder.java
@@ -0,0 +1,24 @@
+package first;
+
+import java.util.Scanner;
+
+public class CafeOrder {
+
+	public static void main(String[] args) {
+	Cafe cafe = new Cafe();
+	System.out.println("현재 잔액을 확인하세요 >>");	
+	Scanner scanner = new Scanner(System.in);
+	cafe.GuestMoney = scanner.nextInt();
+	System.out.println("[현재 잔액" + ":" + cafe.GuestMoney + "]");
+	System.out.println("카페에 들어왔습니다. 주문을 합시다.\n");
+	
+	cafe.cafeMenu();
+	cafe.orderCoffee();
+	
+	
+			
+		
+	
+	}//main
+	
+}//end class
