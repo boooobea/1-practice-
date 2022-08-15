27f78bf7ebb5d67d1378d9773d80867565700b50
bobae <childhop90@gmail.com>
2022-08-15 17:46:41 +0900

    Cafe ----- 2

--- src/first/CafeOrder.java
+++ src/first/CafeOrder.java
@@ -9,7 +9,7 @@ public class CafeOrder {
 	System.out.println("현재 잔액을 확인하세요 >>");	
 	Scanner scanner = new Scanner(System.in);
 	cafe.GuestMoney = scanner.nextInt();
-	
+	 
 	System.out.println("[현재 잔액" + ":" + cafe.GuestMoney + "]");
 	System.out.println("카페에 들어왔습니다. 주문을 합시다.\n");
 	
